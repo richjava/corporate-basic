d4eb8111ce94e1e1f1b1d8161d423e614e14da8e
Richard Lovell <richardlovell@Richards-MacBook-Air.local>
2024-09-11 23:31:41 +1200

    Minor updates

---
+++ lib/theme/constants.ts
@@ -0,0 +1,26 @@
+export const pages = {
+  HOME: "home",
+  SERVICES: "services",
+  BLOG: "blog",
+  BLOG_ARTICLE: "blogArticle",
+  AUTHOR_ARTICLE: "authorArticle",
+  CONTACT: "contact",
+  ABOUT: "about",
+  COVERS: "covers",
+  BANNERS: "banners",
+  BLOCKS: "blocks",
+  CARDS: "cards",
+  FORMS: "forms",
+  LISTS: "lists",
+};
+
+export const pageTypes = {
+  SITE: "site",
+  TEMPLATES: "templates",
+};
+
+export const contentTypes = {
+  POST: "post"
+};
+
+export const language = 'typescript';
---
+++ lib/theme/page.tsx
@@ -0,0 +1,70 @@
+import React, {useEffect, useState} from 'react';
+import {useRouter} from 'next/router';
+import { useParams } from 'next/navigation'
+import Layout from '@/components/plugins/gh_richjava_blog/layout';
+import {getComponents} from '@/builtjs-utils';
+import { setupCrumbs } from ".";
+const {transformPage, fetchEntry, fetchEntries} = require('@builtjs/theme');
+
+const Page = ({config}: any) => {
+
+  const router = useRouter();
+  const params = useParams();
+  const {slug} = router.query;
+  const [page, setPage] = useState<any>(null);
+  const [sectionComps, setSectionComps] = useState<React.ComponentType[]>([]);
+  const [layoutComps, setLayoutComps] = useState<React.ComponentType[]>([]);
+  let [isSetUpCrumbs, setIsSetupCrumbs] = useState(false);
+
+  useEffect(() => {
+    if (!isSetUpCrumbs) {
+      setupCrumbs(router);
+      setIsSetupCrumbs(true);
+    }
+    setPage(null);
+    setLayoutComps([]);
+    init();
+  }, [slug]);
+
+  async function init() {
+    if (!config) {
+      return;
+    }
+    let page: any = await transformPage(config, params);
+    if (!page) {
+      return;
+    }
+    let [sectionComponents, layoutComponents] = await Promise.all([
+      getComponents(page.sections),
+      getComponents(page.layout.sections),
+    ]);
+    setPage(page);
+    setSectionComps(sectionComponents);
+    setLayoutComps(layoutComponents);
+  }
+
+  return (
+    <>
+      <Layout layoutComps={layoutComps} page={page}>
+        {
+          <>
+            {page &&
+              sectionComps.length > 0 &&
+              sectionComps.map((Section: any, i: number) => {
+                return (
+                  page.sections[i] && (
+                    <Section 
+                    key={i} 
+                    api={page.sections[i].template.doc.type === 'dynamic' ? {fetchEntry, fetchEntries} : null} 
+                    content={page.sections[i].content} />
+                  )
+                );
+              })}
+          </>
+        }
+      </Layout>
+    </>
+  );
+};
+
+export default Page;
