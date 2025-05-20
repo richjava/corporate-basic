0af6666b3092e54dcf7dd9baea065c9c2cf4a6cb
Richard Lovell <devops@builtjs.com>
2025-05-20 14:58:48 +1200

    Updated btn

--- components/elements/button.tsx
+++ components/elements/button.tsx
@@ -1,8 +1,22 @@
 export default function Button({ data }:any) {
   if (!data) return <></>;
+  const isPrimary = data.type === "primary";
+
+  const primaryClasses =
+    "bg-gray-200 text-black hover:bg-gray-300 dark:bg-white dark:text-black dark:hover:bg-gray-100";
+
+  const secondaryClasses =
+    "bg-white text-black hover:bg-gray-100 dark:bg-gray-200 dark:text-black dark:hover:bg-gray-300";
+
+  const baseClasses =
+    "font-semibold py-2 px-4 rounded transition-colors duration-200";
+
+  const buttonClass = `${baseClasses} ${
+    isPrimary ? primaryClasses : secondaryClasses
+  }`;
   return (
-    <div id="form-button" className={`btn btn-${data.type} w-full`}>
+    <button className={buttonClass}>
       {data.label}
-    </div>
+    </button>
   );
 }
